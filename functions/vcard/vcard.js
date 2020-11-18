96ff97fc7a8d088b30cc2c7b1f60ed4929b81d99
hasansattar <hasansattar650@gmail.com>
2020-11-18 20:38:50 +0500

    send data to faunadb with graphql mutation

---
+++ functions/vcard/vcard.js
@@ -0,0 +1,118 @@
+const { ApolloServer, gql } = require('apollo-server-lambda')
+var faunadb = require('faunadb'),
+  q = faunadb.query;
+  const shortid = require('shortid');
+
+
+const typeDefs = gql`
+  type Query {
+         getVCard: [vCard]
+      }
+
+      
+  type vCard {
+    id: ID!
+    c1:String!
+    c2:String!
+    c3:String!
+    rec:String!
+    sender:String!
+    msg:String! 
+    Link:String!
+  }
+
+
+  type Mutation{
+    addVCard(c1:String!,
+      c2:String!,c3:String!,
+      rec:String!,sender:String!,
+      msg:String! ) :vCard
+     }
+`
+
+ 
+
+const resolvers = {
+  Query: {
+     
+     getVCard: async () => {
+
+      try{
+        var adminClient = new faunadb.Client({ secret: 'fnAD6USJAlACAZclbn1CG-tLg-lvV7mp_sCEwD9e' });
+     
+        const result = await adminClient.query(
+          q.Map(
+            q.Paginate(q.Match(q.Index('lollyindex'))),
+            q.Lambda(x => q.Get(x))
+          )
+        )
+
+        console.log(result)
+
+         return result.data.map(d=>{
+           return {
+             id:d.ts,
+             c1:d.data.c1,
+             c2:d.data.c2,
+             c3:d.data.c3,
+             rec:d.data.rec,
+             sender:d.data.sender,
+             msg:d.data.msg
+           }
+         })
+
+      }
+      catch(error){
+        console.log(error);
+
+      }
+
+
+       
+    }
+     
+  },
+  Mutation:{
+    addVCard: async (_,{c1,c2,c3,rec,sender,msg})=>{
+    console.log("============")
+    console.log(c1,c2,c3,rec,sender,msg)
+    
+    try {
+      var adminClient = new faunadb.Client({ secret: 'fnAD6USJAlACAZclbn1CG-tLg-lvV7mp_sCEwD9e' });
+
+
+        const result = await adminClient.query(
+
+          q.Create(
+            q.Collection('lollycollection'),
+            {
+              data: {
+                c1,c2,c3,rec,sender,msg,
+                link: shortid.generate()
+              }
+            },
+          )
+
+        )
+        console.log(result.data)
+
+        return result.data.data;
+
+
+    } catch (error) {
+      console.log(error)
+      
+    }
+    
+    
+   
+  }
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+exports.handler = server.createHandler()
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,58 @@
 import React, { useRef, useState } from "react"
 import Lolly from "../components/lolly"
 import './style.css'
+import {useQuery,useMutation} from '@apollo/client';
+import gql from 'graphql-tag';
+
+ 
+const GET_DATA=gql`{
+  getVCard{
+    id
+     c1
+     c2
+     c3
+     msg
+     rec
+     sender
+    
+  }
+}`;
+
+const ADD_DATA=gql`
+mutation addVCard($c1:String!,$c2:String!,$c3:String!,$rec:String!,$sender:String!,$msg:String!){
+     addVCard(c1:$c1,c2:$c2,c3:$c3,rec:$rec,sender:$sender,msg:$msg){
+       id
+     }
+}
+`
+
+
 
 export default function Home() {
-  
+
+
   const [c1, setC1] = useState("#deaa43");
   const [c2, setC2] = useState("#e95946");
   const [c3, setC3] = useState("#d52358");
 
 
+const [addVCard]=useMutation(ADD_DATA);
+
   const handleSubmit = () => {
     console.log(senderField.current.value);
     console.log(msgField.current.value);
     console.log(receiverField.current.value);
+     addVCard({
+       variables:{
+         c1:c1,
+         c2:c2,
+         c3:c3,
+         rec:receiverField.current.value ,
+         sender:senderField.current.value,
+         msg:msgField.current.value
 
+       }
+     })
 
   }
 
@@ -21,6 +60,20 @@ export default function Home() {
   const senderField = useRef();
   const receiverField = useRef();
   const msgField = useRef();
+
+
+   
+
+
+  const {loading,error,data}= useQuery(GET_DATA);
+  console.log(data)
+  if(loading)
+  return <h2>loading..</h2>
+  if(error)
+  return <h2>error</h2>
+  
+ 
+
 
 
   return (
@@ -41,6 +94,9 @@ export default function Home() {
           <input type="text" placeholder="Form" ref={senderField} />
 
           <button onClick={handleSubmit} >Send</button>
+
+
+         
         </div>
       </div>
     </div>
